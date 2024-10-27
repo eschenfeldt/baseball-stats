0ae9b2f3b979819fc75c0705ebd9a8f2013b132f
Patrick Eschenfeldt <peschenfeldt@icloud.com>
2024-10-27 17:02:32 -0400

    games tab mobile improvements

--- baseball-app/src/app/games/games-datasource.ts
+++ baseball-app/src/app/games/games-datasource.ts
@@ -1,35 +1,8 @@
 import { ApiMethod, BaseballApiService } from '../baseball-api.service'
 import { BaseballDataSource } from '../baseball-data-source'
 import { BaseballApiFilter, BaseballFilterService } from '../baseball-filter.service'
-import { GameType } from '../contracts/game-type'
+import { GameSummary } from '../contracts/game-summary';
 import { PagedApiParameters } from '../paged-api-parameters'
-import { Park } from '../contracts/park'
-import { Player } from '../contracts/player'
-import { Team } from '../contracts/team'
-
-
-export interface GameSummary {
-    id: number,
-    externalId: number,
-    name: string,
-    date: Date,
-    gameType?: GameType,
-    home: Team,
-    homeTeamName: string,
-    away: Team,
-    awayTeamName: string,
-    scheduledTime?: string,
-    startTime?: string,
-    endTime?: string,
-    location?: Park,
-    homeScore: number,
-    awayScore: number,
-    winningTeam?: Team,
-    losingTeam?: Team,
-    winningPitcher?: Player,
-    losingPitcher?: Player,
-    savingPitcher?: Player
-}
 
 export interface GamesListParams extends PagedApiParameters {
     teamId?: number;
@@ -49,5 +22,4 @@ export class GamesDataSource extends BaseballDataSource<GamesListParams, GameSum
     protected override getParameters(): GamesListParams {
         return {};
     }
-
 }
